66a11c747f6e429c8164c3ed75e7f3298815872d
Tom <thomas.spencer@codurance.com>
2020-05-24 13:38:31 +0100

    Add principle12 fe for cors filter settings

--- src/main/java/com/codurance/principle12/Retropolis.java
+++ src/main/java/com/codurance/principle12/Retropolis.java
@@ -26,7 +26,7 @@ public class Retropolis {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com"));
+        config.setAllowedOrigins(Collections.singletonList("http://principle12-fe.s3-website.eu-west-2.amazonaws.com"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
